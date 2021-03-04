19650258f28060eabfacc86f3ca4142375e3fefc
my pc <niteblock@gmail.com>
2021-03-04 20:09:50 +0100

    add routers, too server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const brawlStars = require('./routers/brawlStars.js');
+const clashOfClans = require('./routers/clashOfClans.js');
+const clashRoyale = require('./routers/clashRoyale.js');
+const hypixel = require('./routers/hypixel.js');
 
 app.use(cors());
+app.use('/brawlStars', brawlStars);
+app.use('/clashOfClans', clashOfClans);
+app.use('/clashRoyale', clashRoyale);
+app.use('/hypixel', hypixel);
 
 app.get("/", (req, res) => {
   res.send({
