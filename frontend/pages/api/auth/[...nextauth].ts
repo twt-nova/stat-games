8b0e5b5e59aabd2757bbd41839271df6a49aa6d8
Amel Islamovic <aislqmovic@gmail.com>
2021-03-05 13:53:36 +0100

    added mongodb to auth

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -2,18 +2,18 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { NextApiHandler } from "next";
 
-console.log(process.env.GITHUB_ID);
+console.log();
 
 const authHandler: NextApiHandler = (req, res) =>
   NextAuth(req, res, {
     providers: [
       Providers.GitHub({
-        clientId: "ca3f8474364261441f5c",
-        clientSecret: "0fb978efcae717f9a26818a84643b32ea3ae1784",
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
       }),
     ],
 
-    database: "mysql://root:rubikovakocka123A!@127.0.0.1:3306/statgames",
+    database: process.env.DATABASE_URL,
   });
 
 export default authHandler;
