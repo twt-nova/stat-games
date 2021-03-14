1ad0f946a5da03cb2e058946988373cf0126be2c
Nite <Niteblock@gmail.com>
2021-03-14 14:33:08 +0100

    hypixel Skywars and clash of clans updates (#22)
    * hypixel Skywars and clash of clans updates
    
    * add updates to hypixel

--- backend/routes/hypixel.js
+++ backend/routes/hypixel.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const hypixelAPI = "https://api.hypixel.net";
-const KEY = process.env.HYPIXEL_KEY;
-const {
-  fetchFrom,
-  getLimitQuery,
-  ensureUUID,
-  fetchFromWithParams,
-} = require("../utils/routesUtils");
 
-const { formatBedwars } = require("../utils/hypixelUtilsBedwars");
+const hypixel = require("../functions/hypixel");
 
 router.get("/", (req, res) => {
   res.json({
@@ -20,30 +12,27 @@ router.get("/", (req, res) => {
 
 router.get("/player/:name/", async (req, res) => {
   const name = req.params.name;
-  const uuid = await ensureUUID(name);
-  let player = await getPlayerByUUID(uuid);
-  delete player.player.stats;
+  let player = await hypixel.getPlayerByUUID(name);
   res.json(player.player);
+});
+
+router.get("/player/:name/raw", async (req, res) => {
+  const name = req.params.name;
+  let player = await hypixel.getPlayerByUUID(name);
+  res.json(player);
 });
 
 router.get("/player/:name/bedwars/", async (req, res) => {
   const name = req.params.name;
-  const uuid = await ensureUUID(name);
-  let player = await getPlayerByUUID(uuid);
-  let data = formatBedwars(player.player.stats.Bedwars)
+  let data = await hypixel.getBedwarsByUUID(name);
   res.json(data);
 });
 
 router.get("/player/:name/skywars/", async (req, res) => {
   const name = req.params.name;
-  const uuid = await ensureUUID(name);
-  let player = await getPlayerByUUID(uuid);
-  res.json(player.player.stats.SkyWars);
+  let data = await hypixel.getSkyWarsByUUID(name);
+  res.json(data);
 });
 
-async function getPlayerByUUID(uuid) {
-  const url = `${hypixelAPI}/player`;
-  return await fetchFromWithParams(url, { key: KEY, uuid: uuid });
-}
 
 module.exports = router;
