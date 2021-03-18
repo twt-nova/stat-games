19fdd5185c336449ac4a3886a11bdd9c27a05204
Nite <Niteblock@gmail.com>
2021-03-18 11:43:39 +0100

    Backend (#44)
    * add some things
    
    * fix rankcolor
    
    * fix ++ color
    
    * typo lmao
    
    * add brawlstars stuff

--- discordbot/utils/fetch_from.py
+++ discordbot/utils/fetch_from.py
@@ -1,8 +1,9 @@
 import aiohttp
 import asyncio
-
+import json
 
 async def fetch_from(url, bot):
     session = bot.session or aiohttp.ClientSession()
     async with session.get(url) as resp:
-        return await resp.json()
+        data = await resp.read()
+    return json.loads(data)
