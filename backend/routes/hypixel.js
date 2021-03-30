ce23a65b1b3affe5b7345ce5de5498bd331fd473
Nite <Niteblock@gmail.com>
2021-03-30 18:04:38 +0200

    Backend (#65)
    * PAIMON IS HUNGRY
    
    * add genshin
    
    * add charts for stats
    
    * add 404 response code when not found and fix stuff
    
    Co-authored-by: Gerson <Gers2017@outlook.com>

--- backend/routes/hypixel.js
+++ backend/routes/hypixel.js
@@ -13,30 +13,45 @@ router.get("/", (req, res) => {
 router.get("/player/:name/", async (req, res) => {
   const name = req.params.name;
   let player = await hypixel.getPlayerCleanByUUID(name);
+  if (!player.displayName) {
+    res.status(404).send(player)
+  }
   res.json(player);
 });
 
 router.get("/player/:name/raw", async (req, res) => {
   const name = req.params.name;
   let player = await hypixel.getPlayerByUUID(name);
+  if (!player.displayname) {
+    res.status(404).send(player)
+  }
   res.json(player);
 });
 
 router.get("/player/:name/bedwars/", async (req, res) => {
   const name = req.params.name;
   let data = await hypixel.getBedwarsByUUID(name);
+  if (!data.overall.wins_bedwars) {
+    res.status(404).send(data)
+  }
   res.json(data);
 });
 
 router.get("/player/:name/skywars/", async (req, res) => {
   const name = req.params.name;
   let data = await hypixel.getSkyWarsByUUID(name);
+  if (!data.overall.solo_normal.kills) {
+    res.status(404).send(data)
+  }
   res.json(data);
 });
 
 router.get("/player/:name/guild/", async (req, res) => {
   const name = req.params.name;
   let data = await hypixel.getGuildByUUID(name);
+  if (!data.name) {
+    res.status(404).send(data)
+  }
   res.json(data);
 });
 
